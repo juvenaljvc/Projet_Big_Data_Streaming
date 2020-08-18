c4512b930f7ccf94b6f02156a53862f80a944dd1
Juvenal JVC <jvc35@yahoo.fr>
2020-08-18 14:33:52 +0200

    développement d'un Producer Kafka pour Twitter Streaming avec hosebird - complet

--- src/main/Scala/TwitterKafkaStreaming.scala
+++ src/main/Scala/TwitterKafkaStreaming.scala
@@ -12,8 +12,22 @@ import com.twitter.hbc.core.processor.StringDelimitedProcessor
 
 import scala.collection.JavaConverters._
 import KafkaStreaming._
+import org.apache.log4j.{LogManager, Logger}
 
 class TwitterKafkaStreaming {
+
+  private var trace_client_streaming : Logger = LogManager.getLogger("Log_Console")
+
+  /**
+   * Ce client est un client Hosebird. Il permet de collecter les tweets contenant une liste d'hashtag et de les publier en temps réel dans un ou plusieurs topics kafka
+   * @param CONSUMER_KEY : la clé du consommateur pour l'authentification OAuth
+   * @param CONSUMER_SECRET : le secret du consommateur pour l'authentification OAuth
+   * @param ACCESS_TOKEN : le token d'accès pour l'authentification OAuth
+   * @param TOKEN_SECRET : le token secret pour l'authentification OAuth
+   * @param liste_hashtags : la liste des hastags de tweets dont on souhaite collecter
+   * @param kafkaBootStrapServers : la liste d'adresses IP (et leur port) des agents du cluster Kafka
+   * @param topic : le(s) topic(s) dans le(s)quel(s) stocker les tweets collectés
+   */
 
   def ProducerTwitterKafkaHbc (CONSUMER_KEY : String, CONSUMER_SECRET : String, ACCESS_TOKEN : String, TOKEN_SECRET : String, liste_hashtags : String,
                                kafkaBootStrapServers : String, topic : String) : Unit = {
@@ -35,14 +49,25 @@ class TwitterKafkaStreaming {
       )
 
     val client_hbc : Client = constructeur_hbc.build()
-    client_hbc.connect()
 
-    while (!client_hbc.isDone) {
-      val tweets : String = queue.poll(15, TimeUnit.SECONDS)
-      getProducerKafka(kafkaBootStrapServers, topic, tweets)     // intégration avec notre producer Kafka
-      println( "message Twitter : " + tweets)
+    try {
+      client_hbc.connect()
+
+      while (!client_hbc.isDone) {
+        val tweets : String = queue.poll(15, TimeUnit.SECONDS)
+        getProducerKafka(kafkaBootStrapServers, topic, tweets)     // intégration avec notre producer Kafka
+        println( "message Twitter : " + tweets)
+      }
+    } catch {
+
+      case ex : InterruptedException => trace_client_streaming.error("le client Twitter HBC a été interrompu à cause de cette erreur : " + ex.printStackTrace())
+
+    } finally {
+
+      client_hbc.stop()
+      getProducerKafka(kafkaBootStrapServers, topic, "").close()
+
     }
-
 
   }
 
