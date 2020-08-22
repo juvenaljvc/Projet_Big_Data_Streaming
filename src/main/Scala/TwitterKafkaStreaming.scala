f7776b756837d5f65ea32e1c6916dca9c0dfd62a
Juvenal JVC <jvc35@yahoo.fr>
2020-08-22 02:47:35 +0200

    développement d'un Producer Kafka pour Twitter Streaming avec Spark Streaming

--- src/main/Scala/TwitterKafkaStreaming.scala
+++ src/main/Scala/TwitterKafkaStreaming.scala
@@ -15,10 +15,30 @@ import com.twitter.hbc.core.processor.StringDelimitedProcessor
 import scala.collection.JavaConverters._
 import KafkaStreaming._
 import org.apache.log4j.{LogManager, Logger}
+import org.apache.spark.streaming.twitter.TwitterUtils
+import twitter4j.auth.OAuthAuthorization
+import SparkBigData._
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.spark.streaming.Minutes
 
 class TwitterKafkaStreaming {
 
   private var trace_client_streaming : Logger = LogManager.getLogger("Log_Console")
+
+  private def  twitterOAuthConf (CONSUMER_KEY : String, CONSUMER_SECRET : String, ACCESS_TOKEN : String, TOKEN_SECRET : String) : ConfigurationBuilder =  {
+
+    val twitterConfig = new ConfigurationBuilder()
+    twitterConfig
+      .setJSONStoreEnabled(true)
+      .setDebugEnabled(true)
+      .setOAuthConsumerKey(CONSUMER_KEY)
+      .setOAuthConsumerSecret(CONSUMER_SECRET)
+      .setOAuthAccessToken(ACCESS_TOKEN)
+      .setOAuthAccessTokenSecret(TOKEN_SECRET)
+
+    return twitterConfig
+
+  }
 
   /**
    * Ce client est un client Hosebird. Il permet de collecter les tweets contenant une liste d'hashtag et de les publier en temps réel dans un ou plusieurs topics kafka
@@ -88,16 +108,7 @@ class TwitterKafkaStreaming {
 
     val queue : BlockingQueue[Status] = new LinkedBlockingQueue[Status](10000)
 
-    val twitterConf : ConfigurationBuilder = new ConfigurationBuilder()
-    twitterConf
-      .setJSONStoreEnabled(true)
-      .setDebugEnabled(true)
-      .setOAuthConsumerKey(CONSUMER_KEY)
-      .setOAuthConsumerSecret(CONSUMER_SECRET)
-      .setOAuthAccessToken(ACCESS_TOKEN)
-      .setOAuthAccessTokenSecret(TOKEN_SECRET)
-
-    val twitterStream  = new TwitterStreamFactory(twitterConf.build()).getInstance()
+    val twitterStream  = new TwitterStreamFactory(twitterOAuthConf(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, TOKEN_SECRET).build()).getInstance()
 
     val listener = new StatusListener {
       override def onStatus(status: Status): Unit = {
@@ -134,9 +145,86 @@ class TwitterKafkaStreaming {
     getProducerKafka(kafkaBootStrapServers, "", "").close()
     twitterStream.shutdown()
 
+  }
+
+  /**
+   *  Client Spark Streaming Twitter Kafka. Ce client Spark Streaming se connecte à Twitter et publie les infos dans Kafka via un Producer Kafka
+   * @param CONSUMER_KEY
+   * @param CONSUMER_SECRET
+   * @param ACCESS_TOKEN
+   * @param TOKEN_SECRET
+   * @param filtre
+   * @param kafkaBootStrapServers
+   * @param topic
+   */
+  def ProducerTwitterKafkaSpark (CONSUMER_KEY : String, CONSUMER_SECRET : String, ACCESS_TOKEN : String, TOKEN_SECRET : String, filtre : Array[String],
+                                 kafkaBootStrapServers : String, topic : String) : Unit = {
+
+    val authO = new OAuthAuthorization(twitterOAuthConf(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, TOKEN_SECRET).build())
+
+    val client_Streaming_Twitter = TwitterUtils.createStream(getSparkStreamingContext(true, 15), Some(authO), filtre)
+
+    val tweetsmsg = client_Streaming_Twitter.flatMap(status => status.getText())
+    val tweetsComplets = client_Streaming_Twitter.flatMap(status => (status.getText() ++ status.getContributors() ++ status.getLang()))
+    val tweetsFR = client_Streaming_Twitter.filter(status => status.getLang() == "fr")
+    val hastags = client_Streaming_Twitter.flatMap(status => status.getText().split(" ").filter(status => status.startsWith("#")))
+    val hastagsFR = tweetsFR.flatMap(status => status.getText().split(" ").filter(status => status.startsWith("#")))
+    val hastagsCount = hastagsFR.window(Minutes(3))
+
+    // ATTENTION à cette erreur !!! getProducerKafka(kafkaBootStrapServers, topic, tweetsmsg.toString())
+
+    // tweetsmsg.saveAsTextFiles("Tweets")
+
+    tweetsmsg.foreachRDD {
+    (tweetsRDD, temps) =>
+      if (!tweetsRDD.isEmpty()) {
+        tweetsRDD.foreachPartition {
+          partitionsOfTweets =>
+            val producer_Kafka = new KafkaProducer[String, String](getKafkaProducerParams(kafkaBootStrapServers))
+            partitionsOfTweets.foreach {
+              tweetEvent =>
+                val record_publish = new ProducerRecord[String, String](topic, tweetEvent.toString)
+                producer_Kafka.send(record_publish)
+
+            }
+            producer_Kafka.close()
+        }
+      }
+  }
+
+    try {
+      tweetsComplets.foreachRDD {
+        tweetsRDD =>
+          if (!tweetsRDD.isEmpty()) {
+            tweetsRDD.foreachPartition{
+              tweetsPartition => tweetsPartition.foreach{ tweets =>
+                getProducerKafka(kafkaBootStrapServers, topic, tweets.toString )
+              }
+            }
+          }
+          getProducerKafka(kafkaBootStrapServers, "", "").close()
+      }
+    }
+    catch {
+      case ex : Exception => trace_client_streaming.error(ex.printStackTrace())
+    }
+
+    getSparkStreamingContext(true, 15).start()
+    getSparkStreamingContext(true, 15).awaitTermination()
+
+    // pour arrêter le contexte Spark Streaming : getSparkStreamingContext(true, 15).stop()
 
   }
 
+
+  // exemple de spécification d'un paramètre optionel en scala
+  test(Some(true), 15)
+
+  // exemple de spécification d'un paramètre optionel en scala
+  def test (var1 : Option[Boolean], param2 : Int): Unit = {
+    println(var1)
+
+  }
 
 
 }
