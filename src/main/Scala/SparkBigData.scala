25664c00f56dbeeb70e4c50b9d7478bb95deec62
Juvenal JVC <jvc35@yahoo.fr>
2020-09-18 02:32:27 +0200

    Maîtrisez les bases du développement d'application en Spark

--- src/main/Scala/SparkBigData.scala
+++ src/main/Scala/SparkBigData.scala
@@ -6,6 +6,9 @@ import org.apache.spark.sql._
 import org.apache.spark.streaming._
 import org.apache.log4j._
 import org.apache.log4j.LogManager
+import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
+
 
 object SparkBigData {
 
@@ -15,6 +18,89 @@ object SparkBigData {
    var spConf : SparkConf = null
 
    private var trace_log : Logger = LogManager.getLogger("Logger_Console")
+
+  def main(args: Array[String]): Unit = {
+
+    val session_s = Session_Spark(true)
+    val sc = session_s.sparkContext
+
+    sc.setLogLevel("OFF")
+
+    val rdd_test : RDD[String] = sc.parallelize(List("alain", "juvenal", "julien", "anna"))
+
+    rdd_test.foreach{
+      l =>  println(l)
+    }
+    println()
+
+    val rdd2 : RDD[String] = sc.parallelize(Array("Lucie", "fabien", "jules"))
+    rdd2.foreach{ l => println(l)}
+    println()
+
+    val rdd3 = sc.parallelize(Seq(("Julien", "Math", 15), ("Aline", "Math", 17), ("Juvénal", "Math", 19)))
+    println("Premier élément de mon RDD 3")
+    rdd3.take(1).foreach{ l => println(l)}
+
+    if(rdd3.isEmpty()) {
+      println("le RDD est vide")
+    } else {
+      rdd3.foreach{
+        l => println(l)
+      }
+    }
+
+    // rdd3.saveAsTextFile("F:\\Mes publications\\Mes publications\\Complete\\formation Spark Big Data\\Ressources\\rdd.txt")
+
+    // rdd3.repartition(1).saveAsTextFile("F:\\Mes publications\\Mes publications\\Complete\\formation Spark Big Data\\Ressources\\rdd3.txt")
+
+    rdd3.foreach{l => println(l)}
+   // rdd3.collect().foreach{l => println(l)}
+
+    // création d'un RDD à partir d'une source de données
+    val rdd4 = sc.textFile("F:\\Mes publications\\Mes publications\\Complete\\formation Spark Big Data\\Ressources\\Sources\\textRDD.txt")
+    println("lecture du contenu du RDD4")
+   // rdd4.foreach{l => println(l)}
+
+    val rdd5 = sc.textFile("F:\\Mes publications\\Mes publications\\Complete\\formation Spark Big Data\\Ressources\\Sources\\*")
+    println("lecture du contenu du RDD5")
+   // rdd5.foreach{l => println(l)}
+
+    // transformation des RDD
+    val rdd_trans : RDD[String] = sc.parallelize(List("alain mange une banane", "la banane est un bon aliment pour la santé", "acheter une bonne banane"))
+    rdd_trans.foreach(l => println( "ligne de mon RDD : " + l))
+
+    val rdd_map = rdd_trans.map(x => x.split(" "))
+    println("Nbr d'elements de mon RDD Map : " + rdd_map.count())
+
+    val rdd6 = rdd_trans.map(w => (w, w.length, w.contains("banane"))).map(x => (x._1.toLowerCase, x._2, x._3))
+
+    val rdd7 = rdd6.map(x => (x._1.toUpperCase(), x._3, x._2))
+
+    val rdd8 = rdd6.map(x => (x._1.split(" "), 1) )
+    rdd8.foreach(l => println(l._1(0), l._2))
+
+    val rdd_fm = rdd_trans.flatMap(x => x.split(" ")).map(w => (w, 1))
+
+    val rdd_compte = rdd5.flatMap(x => x.split(" ")).map(m => (m, 1)).reduceByKey((x, y) => x + y)
+    rdd_compte.repartition(1).saveAsTextFile("F:\\Mes publications\\Mes publications\\Complete\\formation Spark Big Data\\Ressources\\Sources\\ComptageRDD.txt")
+    rdd_compte.foreach(l => println(l))
+
+    val rdd_filtered = rdd_fm.filter(x => x._1.contains("banane"))
+
+    val rdd_reduced = rdd_fm.reduceByKey((x, y) => x + y)
+   // rdd_reduced.foreach(l => println(l))
+
+    rdd_fm.cache()
+    rdd_fm.persist(StorageLevel.MEMORY_AND_DISK)
+    rdd_fm.unpersist()
+
+    import session_s.implicits._
+    val df : DataFrame = rdd_fm.toDF("texte", "valeur")
+    df.show(50)
+
+
+
+  }
 
 
   /**
@@ -47,6 +133,7 @@ object SparkBigData {
     return ss
 
   }
+
 
   /**
    * fonction qui initialise le contexte Spark Streaming
